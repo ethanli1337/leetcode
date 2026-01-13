2724941ddb87289944de314eb243505c645769bf
Ethan Li <ethanli2756@gmail.com>
2026-01-12 22:29:59 -0600

    two sum 2 pointers

--- twosum.py
+++ twosum.py
@@ -20,3 +20,19 @@ def two_sum_optimized(nums, target):
             hash_map[num] = i 
 
 print(two_sum_optimized(nums,target))
+
+def two_sum_2_pointers(nums, target):
+    n=len(nums)
+    l=0
+    r=n-1 
+
+    while l<r:
+        sum=nums[l] + nums[r]
+        if sum==target:
+            return [l,r]
+        elif sum < target:
+            l+=1 
+        else:
+            r-=1 
+        
+print(two_sum_2_pointers(nums,target))
