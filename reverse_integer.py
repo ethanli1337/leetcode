3299e19396d1cc9ea1da719de64cb0421e47e12c
Ethan Li <ethanli2756@gmail.com>
2026-02-15 22:25:50 -0600

    reverse int

---
+++ reverse_integer.py
@@ -0,0 +1,12 @@
+def reverse(x:int) -> int:
+    INT_MIN, INT_MAX = -2**31, 2**31-1 
+    result=0
+    negative=x<0
+    x=abs(x)
+    while x != 0:
+        digit=x%10 
+        x=x//10
+        if result > (INT_MAX - digit)//10:
+            return 0 
+        result=result*10+digit
+    return -result if negative else result
